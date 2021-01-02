65ecd7287c966cde6927f9e21a7628c3526cce37
Pierre <pierrelejay1@gmail.com>
2021-01-02 15:15:43 +0000

    Finished day 9

--- src/day9/solver.js
+++ src/day9/solver.js
@@ -1,8 +1,49 @@
-var test;
-var str = "";
-test = "hello";
-str = str + test;
-function testFunc(a, b) {
-    console.log(str);
+"use strict";
+exports.__esModule = true;
+var fs = require("fs");
+var inputArr = fs
+    .readFileSync('src/day9/input.txt')
+    .toString()
+    .split('\n')
+    .map(function (x) { return Number(x); });
+var preambleSize = 25;
+function solver1(arr) {
+    var previousNums = arr.slice(0, preambleSize);
+    for (var i = preambleSize; i < arr.length; i++) {
+        var num = arr[i];
+        if (!isValidSum(previousNums, num)) {
+            return num;
+        }
+        previousNums.push(num);
+        previousNums.shift();
+    }
+    return 1;
 }
-console.log("test");
+function solver2(arr) {
+    var invalidNum = solver1(arr);
+    for (var i = 0; i < arr.length - 1; i++) {
+        var sum = arr[i];
+        for (var j = i + 1; j < arr.length; j++) {
+            sum += arr[j];
+            if (sum === invalidNum) {
+                var nums = arr.slice(i, j).sort();
+                return nums[0] + nums[nums.length - 1];
+            }
+            else if (sum > invalidNum) {
+                break;
+            }
+        }
+    }
+    return 1;
+}
+function isValidSum(previousNums, num) {
+    for (var i = 0; i < previousNums.length - 1; i++) {
+        for (var j = i + 1; j < previousNums.length; j++) {
+            if (previousNums[i] + previousNums[j] === num)
+                return true;
+        }
+    }
+    return false;
+}
+console.log(solver1(inputArr));
+console.log(solver2(inputArr));
--- src/day9/solver.ts
+++ src/day9/solver.ts
@@ -1,12 +1,51 @@
-let test: number;
-let str = "";
+import * as fs from 'fs';
 
-test = "hello";
+const inputArr: number[] = fs
+  .readFileSync('src/day9/input.txt')
+  .toString()
+  .split('\n')
+  .map((x) => Number(x));
 
-str = str + test;
+const preambleSize = 25;
 
-function testFunc(a, b) {
-  console.log(str);
+function solver1 (arr: number[]): number {
+  const previousNums: number[] = arr.slice(0, preambleSize);
+  for (let i = preambleSize; i < arr.length; i++) {
+    const num = arr[i];
+    if (!isValidSum(previousNums, num)) {
+      return num;
+    }
+    previousNums.push(num);
+    previousNums.shift();
+  }
+  return 1;
 }
 
-console.log("test");
+function solver2 (arr: number[]): number {
+  const invalidNum = solver1(arr);
+  for (let i = 0; i < arr.length - 1; i++) {
+    let sum = arr[i];
+    for (let j = i + 1; j < arr.length; j++) {
+      sum += arr[j];
+      if (sum === invalidNum) {
+        const nums = arr.slice(i, j).sort();
+        return nums[0] + nums[nums.length - 1];
+      } else if (sum > invalidNum) {
+        break;
+      }
+    }
+  }
+  return 1;
+}
+
+function isValidSum (previousNums: number[],num: number): boolean {
+  for (let i = 0; i < previousNums.length - 1; i++) {
+    for (let j = i + 1; j < previousNums.length; j++) {
+      if (previousNums[i] + previousNums[j] === num) return true;
+    }
+  }
+  return false;
+}
+
+console.log(solver1(inputArr));
+console.log(solver2(inputArr));
