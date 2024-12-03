bdc813386c381f9520131ef5d8f72f91def61bb7
Pierre Lejay <pierrelejay1@gmail.com>
2024-12-03 20:21:08 +0000

    2024 day2 - cleanup

--- python/2024/day2/solver.py
+++ python/2024/day2/solver.py
@@ -12,35 +12,6 @@ def is_compliant(val: int, second_val: int, is_descending: bool) -> bool:
     return False
   return True
 
-def old_solver (tolerance_level: int):
-  safe_sum = 0
-
-  for line in lines:
-    split_line = line.split(" ")
-    is_descending = split_line[0] > split_line[1]
-    remaining_tolerance = tolerance_level
-    for i in range(len(split_line)):
-      # If the loop has reached the end of the input without failing, the line is safe
-      if i == len(split_line) - 1:
-        safe_sum += 1
-        break
-
-      val = int(split_line[i])
-      next_val = int(split_line[i + 1])
-      if not is_compliant(val, next_val, is_descending):
-        if remaining_tolerance == 0 or i > len(split_line) - 3:
-          break
-        else:
-          next_next_val = int(split_line[i + 2])
-          if not is_compliant(val, next_next_val, is_descending):
-            break
-          else:
-            remaining_tolerance -= 1
-            continue
-
-
-  return safe_sum
-
 def is_line_safe(levels: list[int]) -> bool:
   is_descending = levels[0] > levels[1]
   for i in range(len(levels)):
@@ -53,10 +24,6 @@ def is_line_safe(levels: list[int]) -> bool:
       break
 
   return False
-
-
-
-
 
 def solver(tolerance_level: int) -> int:
   safe_sum = 0
